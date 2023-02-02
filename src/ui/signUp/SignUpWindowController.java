e097d0c3d4856a35ff93933d046277323e863f7e
optickx <danielbarrios2002@gmail.com>
2023-02-02 13:24:03 +0100

    near to be finished

--- src/ui/signUp/SignUpWindowController.java
+++ src/ui/signUp/SignUpWindowController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -27,51 +26,41 @@ import javafx.scene.layout.AnchorPane;
  */
 public class SignUpWindowController {
 
-    private TextField txtLogin;
+    // FXML elements
 
     @FXML
-    private TextField 
-        txtEmail, txtFullName;
-    private final String
-        txtLoginPromptText = "", txtEmailPromptText = "", 
-        txtFullNamePromptText = "";
-    
+    private TextField txtLogin, txtEmail, txtFullName;
+    private final String 
+        txtLoginPromptText = "Login", txtEmailPromptText = "Email",
+            txtFullNamePromptText = "Full name";
 
     @FXML
-    private PasswordField
-        password, passwordConfirmation;
-    private final String 
-        passwordPromptText = "Passsworrdd pleasee", 
-        passwordConfirmationPromptText = "Repeat the password";
+    private PasswordField password, passwordConfirmation;
+    private final String passwordPromptText = "Passsworrdd pleasee",
+            passwordConfirmationPromptText = "Repeat the password";
 
-
-    @FXML 
-    private Button 
-        btnRegister, btnClear, btnExit;
-    private final String 
-        btnRegisterText = "Register", 
-        btnClearText = "Clear", btnExitText = "Exit";
-
+    @FXML
+    private Button btnRegister, btnClear, btnExit;
+    private final String btnRegisterText = "Register",
+            btnClearText = "Clear", btnExitText = "Exit";
 
     private Stage stage;
 
-    private final String WINDOW_NAME = "AdminUserData"; 
+    private final String WINDOW_NAME = "AdminUserData";
 
     // LOGGER that will be used to note every event of the window.
-    private static final Logger LOGGER =
-        Logger.getLogger("package user;");
+    private static final Logger LOGGER = Logger.getLogger("package user;");
     @FXML
     private AnchorPane AnchorPane;
 
-
-
+    /**
+     * Prepare the stage for a change of scene
+     * 
+     * @param stage where the window shows
+     */
 
     public void setStage(Stage stage) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public void initSignUp(Parent root) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.stage = stage;
     }
 
 
@@ -79,89 +68,78 @@ public class SignUpWindowController {
      * HANLERS OF EVERY POSSIBLE CHANGE IN THE WINDOW ------------------
      */
 
-     /**
-      * if any text from the window changes, go for it.
-      * @param observableValue
-      * @param oldvalue
-      * @param newValue
-      */
+    /**
+     * if any text from the window changes, go for it.
+     * 
+     * @param observableValue
+     * @param oldvalue
+     * @param newValue
+     */
 
-     private void handleTextChanged(
-    ObservableValue observableValue,
-    String oldvalue, String newValue) {
-        /* boolean lesgoo = everyUserParamIsCorrect();
-        
-        btnRegister.setDisable(!lesgoo); */
-
-
-
-
+    private void handleTextChanged(
+            ObservableValue observableValue,
+            String oldvalue, String newValue) {
+        /*
+         * boolean lesgoo = everyUserParamIsCorrect();
+         * 
+         * btnRegister.setDisable(!lesgoo);
+         */
 
     }
 
     /**
      * manage message (user already in use.)
+     * 
      * @return
      */
 
     /**
      * TODO: checking should be correct
      * puts every prompt text
+     * 
      * @return true if every user param is full,
-     * and with the correct format.
+     *         and with the correct format.
      */
 
-     private boolean everyUserParamIsCorrect() {
-        boolean 
-            nicely = true;
+    private boolean everyUserParamIsCorrect() {
+        boolean nicely = true;
 
         // text checkings
 
         if (txtLogin.getText().isEmpty())
             txtLogin.setPromptText(txtLoginPromptText);
-        else 
+        else
             nicely = nicely && USF.isLoginFormat(txtLogin.getText());
-    
+
         if (txtEmail.getText().isEmpty())
-            txtEmail.setPromptText(txtEmailPromptText);    
+            txtEmail.setPromptText(txtEmailPromptText);
         else
             nicely = nicely && USF.isFullNameFormat(txtFullName.getText());
 
         if (txtFullName.getText().isEmpty())
             txtFullName.setPromptText(txtFullNamePromptText);
         else
-            nicely = nicely && USF.isEmailFormat(txtEmail.getText()); 
+            nicely = nicely && USF.isEmailFormat(txtEmail.getText());
 
         if (password.getText().isEmpty())
             password.setPromptText(passwordPromptText);
-        else
-            if (passwordConfirmation.getText().isEmpty()) 
-                passwordConfirmation.setPromptText(passwordConfirmationPromptText);
-
+        else if (passwordConfirmation.getText().isEmpty())
+            passwordConfirmation.setPromptText(passwordConfirmationPromptText);
 
         nicely = nicely && (password.equals(passwordConfirmationPromptText));
-                
-                
-
-        
 
         // TODO: are both passwords the same?
 
         return nicely;
     }
 
-    private void handlerWindowShowing(WindowEvent event) {
-        LOGGER.info("Initializing UserWindowController::handlerWindowShowing");
-
-        // TODO: set all the texts
-    }
-
     /**
      * void that initiates the whole window.
-     * @param root parent 
+     * 
+     * @param root parent
      */
-     
-     public void initStage(Parent root) {
+
+    public void initStage(Parent root) {
         LOGGER.info("Initializing 'SignUp' window");
         // Stablish scene
         Scene scene = new Scene(root);
@@ -173,13 +151,45 @@ public class SignUpWindowController {
         stage.setOnShowing(this::handlerWindowShowing);
 
         stage.show();
-     }
+    }
 
+    /**
+     * set every element text from the begining
+     * @param event
+     */
 
+    private void handlerWindowShowing(WindowEvent event) {
+        LOGGER.info("Initializing UserWindowController::handlerWindowShowing");
+        // button
 
-    
+        btnRegister.setText(btnRegisterText);
+        btnClear.setText(btnClearText);
+        btnExit.setText(btnExitText);
+
+        clearEverything();
+        // TODO: set all the texts
+    }
+
+    private void clearEverything() {
+        // text
+        txtLogin.clear();
+        txtLogin.setPromptText(txtLoginPromptText);
+        txtEmail.clear();
+        txtEmail.setPromptText(txtEmailPromptText);
+        txtFullName.clear();
+        txtFullName.setPromptText(txtFullNamePromptText);
+
+        // password
+
+        password.clear();
+        password.setPromptText(passwordPromptText);
+        passwordConfirmation.clear();
+        passwordConfirmation.setPromptText(passwordConfirmationPromptText);
+    }
+
     /**
      * checks if the spaces have values
+     * 
      * @return
      */
     private boolean everyUserParamIsEmpty() {
@@ -194,13 +204,12 @@ public class SignUpWindowController {
 
     @FXML
     private void handlerRegisterButtonAction() {
-        
-    }
 
+    }
 
     @FXML
     private void handlerClearButtonAction() {
-
+        clearEverything();
     }
 
     @FXML
@@ -208,5 +217,4 @@ public class SignUpWindowController {
 
     }
 
-    
 }
